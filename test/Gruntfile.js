293eaf49a7542487fedbf0e668e018020663d5fd
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-02 10:35:37 +0100

    chore(build): Fix CI build
    Use chrome

--- test/Gruntfile.js
+++ test/Gruntfile.js
@@ -23,12 +23,12 @@ module.exports = function (grunt) {
           devices: {
             desktop: {
               deviceName: 'desktop',
-              browser: 'firefox',
+              browser: 'chrome',
               size: '1280x800'
             },
             tablet: {
               deviceName: 'tablet',
-              browser: 'firefox',
+              browser: 'chrome',
               size: '768x576'
             }
           }
